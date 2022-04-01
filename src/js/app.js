5036dc2f5a6f960881c3f329ea65098f7bced650
Vadim Kiryanov <vadim.kiryanov@mail.ru>
2022-04-01 18:04:35 +0300

    Добавил оживление бургер-меню и выпадающий список

--- src/js/app.js
+++ src/js/app.js
@@ -5,3 +5,24 @@ flsFunctions.isWebp();
 import Swiper, { Navigation, Pagination } from 'swiper';
 
 const swiper = new Swiper();
+
+// =====
+// Достаем кнопку-бургер
+let headerBurger = document.querySelector('.header-burger');
+// Достаем класс header
+let header = document.querySelector('.header');
+// Достаем класс nav
+let nav = document.querySelector('.nav');
+
+// Ф-я toggle кликом
+headerBurger.addEventListener('click', function () { // На headerBurger вешаю знак события, которые срабатывает при клике(click) и запускает функцию
+    headerBurger.classList.toggle('active'); // Сама ф-я: 1) classList - штука, которая говорит о том, что мы можем добавить или удалить класс к уже имеющемуся классу 2) toggle - позволяет добавить какой-то класс, если его нет или удалить, если он есть
+    header.classList.toggle('active');
+    /* nav.style.display = "block"; */
+    nav.classList.toggle('active');
+})
+
+
+
+
+
