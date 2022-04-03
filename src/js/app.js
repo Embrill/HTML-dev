546a4f584598ae4120a0cb3089f0cbc20961fea2
Vadim Kiryanov <vadim.kiryanov@mail.ru>
2022-04-03 12:41:00 +0300

    Добавил fixed к шапке при скролле

--- src/js/app.js
+++ src/js/app.js
@@ -7,6 +7,7 @@ import Swiper, { Navigation, Pagination } from 'swiper';
 const swiper = new Swiper();
 
 // =====
+// БУРГЕР
 // Достаем кнопку-бургер
 let headerBurger = document.querySelector('.header-burger');
 // Достаем класс header
@@ -14,14 +15,22 @@ let header = document.querySelector('.header');
 // Достаем класс nav
 let nav = document.querySelector('.nav');
 
-// Ф-я toggle кликом
+// Ф-я toggle кликом для БУРГЕРА
 headerBurger.addEventListener('click', function () { // На headerBurger вешаю знак события, которые срабатывает при клике(click) и запускает функцию
     headerBurger.classList.toggle('active'); // Сама ф-я: 1) classList - штука, которая говорит о том, что мы можем добавить или удалить класс к уже имеющемуся классу 2) toggle - позволяет добавить какой-то класс, если его нет или удалить, если он есть
     header.classList.toggle('active');
-    /* nav.style.display = "block"; */
     nav.classList.toggle('active');
 })
 
+// FIXED HEADER
+
+window.onscroll = function fixedHeader() {
+    if (window.pageYOffset > 800) {
+        header.classList.add('fixed');
+    } else {
+        header.classList.remove('fixed');
+    }
+}
 
 
 
